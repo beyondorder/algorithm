78fd9a1cb2b8cf727aedf2e78422759e6789b7ad
moon <moon@moonui-MacBookPro.local>
2025-01-19 19:57:53 +0900

    feat: sort folder

---
+++ sort/Sort.java
@@ -0,0 +1,11 @@
+package algorithm.sort;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 4};
+        for (int i : arr) {
+            System.out.print(i + " ");
+            //
+        }
+    }
+}
