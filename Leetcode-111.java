e5c5d614963c9f37ea26ba3ec617c1d5cdad0688
beyondorder <ohyun_pro@naver.com>
2026-02-15 14:28:48 +0900

    Leetcode 111

---
+++ Leetcode-111.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int depth = 1;
+
+        while (!q.isEmpty()){
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right == null) return depth;
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            depth++;
+        }
+
+        return depth;
+    }
+}
